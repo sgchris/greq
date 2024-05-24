d0c71d99f1875fbc49e2c4ff22bdc9afa85630b2
Gregory Chris <sgchris@gmail.com>
2024-05-24 23:40:36 +0300

    implemented parse headers

--- src/greq_object/greq_header.rs
+++ src/greq_object/greq_header.rs
@@ -8,11 +8,15 @@ struct GreqHeader {
 
 impl GreqHeader {
     pub fn from_string(contents: &str) -> GreqHeader {
-        let greq_header = GreqHeader {
-            original_string: contents.to_string(),
-            headers: HashMap::new(),
-        };
+        let mut parsed_headers: HashMap<String, String> = HashMap::new();
+        contents.split("\r\n").for_each(|line| {
+            let line_parts: Vec<&str> = line.split(":").collect();
+            parsed_headers.insert(line_parts[0].to_string(), line_parts[1].to_string());
+        });
 
-        greq_header
+        GreqHeader {
+            original_string: contents.to_string(),
+            headers: parsed_headers,
+        }
     }
 }
