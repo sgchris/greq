61f31d1f951f82dac12cfdd4544f7e23f2f4dd8e
Gregory Chris <sgchris@gmail.com>
2024-08-30 15:05:35 +0300

    updated header validation

--- src/greq_object/greq_header.rs
+++ src/greq_object/greq_header.rs
@@ -4,23 +4,26 @@
 struct GreqHeader {
     original_string: String,
 
-    // project_name: String, // the name of the project. Will be implemented.
+    project: String,          // the name of the project. Will be implemented.
     output_folder: String,    // path to a destination folder. Default current.
     output_file_name: String, // output filename. default current file name with ".response" extension.
 
-    pfx_certificate: String, // Absolute path to the certificate file (pfx)
+    certificate: String, // Absolute path to the certificate file (pfx) certificate password will be handled later
+    base_request: String,
 }
 
 impl GreqHeader {
-    pub fn from_string(contents: &str) -> Result<GreqHeader, &'static str> {
-        if !GreqHeader::is_valid(contents) {
-            return Err("The provided contents for the header are not valid");
-        }
+    pub fn from_string(contents: &str) -> Result<GreqHeader, String> {
+        // validate the contents
+        GreqHeader::is_valid(contents)?;
 
         // initialize the object
-        let mut greq_header = GreqHeader::default();
-        greq_header.original_string = contents.to_string();
+        let mut greq_header = GreqHeader {
+            original_string: contents.to_string(),
+            ..Default::default()
+        };
 
+        let mut unknown_headers: Vec<&str> = vec![];
         // parse lines and assign properties
         contents.split("\r\n").for_each(|line| {
             let line_parts: Vec<&str> = line.split(":").collect();
@@ -28,43 +31,57 @@ impl GreqHeader {
             let header_value = line_parts[1..].join(":").trim().to_lowercase().to_string();
 
             match header_name.to_lowercase().as_str() {
+                "project" => {
+                    greq_header.project = header_value;
+                }
+                "base-request" => {
+                    greq_header.base_request = header_value;
+                }
                 "output-folder" => {
                     greq_header.output_folder = header_value;
                 }
                 "output-file-name" => {
                     greq_header.output_file_name = header_value;
                 }
-                "pfx-certificate" => {
-                    greq_header.pfx_certificate = header_value;
+                "certificate" => {
+                    greq_header.certificate = header_value;
                 }
-                _ => {}
+                _ => {
+                    unknown_headers.push(header_name);
+                }
             }
         });
+
+        if !unknown_headers.is_empty() {
+            // Return an error with the formatted message
+            return Err(format!("Unknown headers: {}", unknown_headers.join(", ")));
+        }
 
         Ok(greq_header)
     }
 
-    pub fn is_valid(contents: &str) -> bool {
+    // should be public as it's used as static
+    pub fn is_valid(contents: &str) -> Result<bool, String> {
         // empty contents allowed
-        if contents.trim().len() == 0 {
-            println!("empty contents of the header");
-            return true;
+        if contents.trim().is_empty() {
+            return Ok(true);
         }
 
-        let has_errors: bool = contents.split("\r\n").any(|line| {
+        let mut errors: Vec<&str> = vec![];
+        contents.split("\r\n").for_each(|line| {
             // must not be empty
             if line.is_empty() {
-                println!("empty line in the header");
-                true
-            } else if !line.contains(":") {
-                println!("the line in the header does not contain ':' character");
-                true
-            } else {
-                false
+                errors.push("empty line in the header");
+            }
+            if !line.contains(":") {
+                errors.push("the line in the header does not contain ':' character");
             }
         });
 
-        !has_errors
+        return match errors.is_empty() {
+            true => Ok(true),
+            false => Err(errors.join(", ")),
+        };
     }
 }
 
@@ -85,6 +102,6 @@ mod tests {
 
         assert_eq!(result.original_string, test_content);
         assert_eq!(result.output_folder, "/some/folder");
-        assert_eq!(result.pfx_certificate, "c:\\some\\folder\\cert.pfx");
+        assert_eq!(result.certificate, "c:\\some\\folder\\cert.pfx");
     }
 }
